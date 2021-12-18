2659bbf398981c8d3c18b116a02d220e816e053f
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-18 21:09:57 +0100

    :heavy_plus_sign: add Router && :sparkles: set up 2 page (homepage/pokemon list) && :art: add some CSS (tailwind)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import {Routes, Route, NavLink} from "react-router-dom";
+
 import List from "./Containers/List.jsx"
 
 function App() {
+    const activeCSS = "bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700 font-semibold"
+    const inactiveCSS = "bg-white inline-block py-2 px-4 text-gray-400 font-semibold";
     return (
         <>
             <h1 className="text-2xl font-bold">Welcome on Vitkov Pokedex</h1>
-            <List/>
+            <nav>
+                <ul className="flex border-b">
+                    <li className="-mb-px mr-1">
+                        <NavLink to="/" className={({isActive}) => ( isActive ? activeCSS : inactiveCSS )}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="-mb-px mr-1">
+                        <NavLink to="/pokemon" className={({isActive}) => (isActive ? activeCSS : inactiveCSS)}>
+                            Pokemon List
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<>You are on Homepage</>}/>
+                <Route path="/pokemon" element={<List/>}/>
+                <Route path="/pokemon/:name" element={<>Pokemon CNI</>}/>
+            </Routes>
+
         </>
     );
 }
