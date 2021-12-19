f05216ff5a426f4b225c7048b48d7278d1643b21
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-19 13:01:06 +0100

    update Contact to add Modal and Form test

--- src/Containers/Contact.jsx
+++ src/Containers/Contact.jsx
@@ -1,9 +1,42 @@
+import {useState} from "react";
+import ReactModal from "react-modal";
+
 const Contact = () => {
+
+    const [modalDisplay, setModalStatus] = useState(false);
+
+    const handleOpenModal = () => setModalStatus(true);
+    const handleCloseModal = () => setModalStatus(false);
+
     return (
         <>
-        modal contact
+            <div>
+                <button onClick={handleOpenModal}>Contact</button>
+                <ReactModal
+                    isOpen={modalDisplay}
+                    //contentLabel="Minimal Modal Example"
+                >
+                    <button onClick={handleCloseModal}>X fermer</button>
+
+                    <form action="" method="get" className="form-example">
+                        <div className="form-example">
+                            <label htmlFor="name">Enter your name: </label>
+                            <input type="text" name="name" id="name" required/>
+                        </div>
+                        <div className="form-example">
+                            <label htmlFor="email">Enter your email: </label>
+                            <input type="email" name="email" id="email" required/>
+                        </div>
+                        <div className="form-example">
+                            <input type="submit" value="Subscribe!"/>
+                        </div>
+                    </form>
+
+                </ReactModal>
+            </div>
         </>
     )
+
 }
 
 export default Contact;
