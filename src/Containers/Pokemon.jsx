6b497af123bf9021d9c4a95782e280cb29ac760e
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-22 19:13:47 +0100

    :art: update pokemon card

--- src/Containers/Pokemon.jsx
+++ src/Containers/Pokemon.jsx
@@ -1,5 +1,6 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import Button from "../Components/Button";
 
 const Pokemon = () => {
 
@@ -56,44 +57,56 @@ const Pokemon = () => {
 
     return (
         <>
+            <Link to="/pokemon">
+                <Button label="Return to Pokedex"
+                        classList="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"/>
+            </Link>
+
             {Object.keys(pokemon).length !== 0 ?
-                <div className="w-80 bg-violet-200 border-black border-4 rounded-xl p-4 text-black">
+                <div className="flex w-max bg-violet-200 border-black border-4 rounded-xl p-4 text-black">
+                    <div>
+                        <div className="w-90 bg-blue-200 border-black border-4 rounded-xl p-4 text-black">
+                            <h2 className="text-4xl font-bold p-1 text-center">{pokemon.name}</h2>
 
-                    <div className="w-90 bg-yellow-200 border-black border-4 rounded-xl p-4 text-black">
-                        <h2 className="text-4xl font-bold p-1 text-center">{pokemon.name}</h2>
+                            <img className="mx-auto" src={pokemon.sprites.front_default} alt={pokemon.name}/>
 
-                        <img className="mx-auto" src={pokemon.sprites.front_default} alt={pokemon.name}/>
+
+                        </div>
+                        <ul className="flex flex-row">
+                            {pokemon.types.map((type, index) => (
+                                <li key={index}
+                                    className={"border-2 border-black rounded-xl m-2 p-2 " + color_type[type.type.name]}>
+                                    {type.type.name}
+                                </li>
+                            ))}
+                        </ul>
+
+                        <div className="">
+                            <div className="">ID: #{pokemon.id}</div>
+                            <div className="">Weight: {pokemon.weight} lbs</div>
+                            <div className="">Height: {pokemon.height} "</div>
+                        </div>
+
+                        <h4 className="text-2xl font-bold p-1 text-center">Abilities</h4>
+                        <ul className="flex flex-row">
+                            {pokemon.abilities.map((ability, index) => (
+                                <li key={index} className="p-2">
+                                    {ability.ability.name}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
+                    <div>
+                        <h4 className="text-2xl font-bold p-1 text-center">Statistics</h4>
+                        <ul>
+                            {pokemon.stats.map((stat, index) => (
+                                <li key={index} className="m-1 p-1 rounded-xl flex flex-row-reverse">
+                                    {stat.stat.name} : {stat.base_stat}
+                                </li>
+                            ))}
+                        </ul>
 
-                    <span>Weight: {pokemon.weight} lbs</span>
-                    <span>Height: {pokemon.height} "</span>
-
-                    <h4>Abilities</h4>
-                    <ul className="flex flex-row">
-                        {pokemon.abilities.map((ability, index) => (
-                            <li key={index} className="p-2">
-                                {ability.ability.name}
-                            </li>
-                        ))}
-                    </ul>
-
-                    <h4>Statistics</h4>
-                    <ul>
-                        {pokemon.stats.map((stat, index) => (
-                            <li key={index} className="border-black border-2 m-1 p-1 rounded-xl flex flex-row">
-                                {stat.stat.name} : {stat.base_stat}
-                            </li>
-                        ))}
-                    </ul>
-
-                    <h4>Type(s)</h4>
-                    <ul className="flex flex-row">
-                        {pokemon.types.map((type, index) => (
-                            <li key={index} className={"border-2 border-black rounded-xl m-2 p-2 " + color_type[type.type.name]}>
-                                {type.type.name}
-                            </li>
-                        ))}
-                    </ul>
+                    </div>
                 </div>
                 : "Si cela apparait c'est que j'ai toujours pas compris les states (-_-)"}
         </>
