774cfaa3230ba280a55e50decaaa52f145ab0acb
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-26 11:15:07 +0100

    split form and filter list

--- src/containers/Filter.jsx
+++ src/containers/Filter.jsx
@@ -3,6 +3,7 @@ import MainContext from "../contexts/FetchPokedex";
 import {useParams} from "react-router-dom";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
+import FormSelectType from "../components/FormSelectType";
 
 const Filter = () => {
 
@@ -97,40 +98,9 @@ const Filter = () => {
     if (loading) {return(<><Loading/></>)}
 
 
-    const handleChange = (event) => {
-        window.history.pushState('', '', `/type/${event.target.value}`);
-        window.location.reload(false);
-    }
-
     return (
         <>
-            <form>
-                <label className="text-purple-700">
-                    Select a pokemon type :
-                    <select
-                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        onChange={handleChange}>
-                        <option value="normal">Normal</option>
-                        <option value="fighting">Fighting</option>
-                        <option value="flying">Flying</option>
-                        <option value="poison">Poison</option>
-                        <option value="ground">Ground</option>
-                        <option value="rock">Rock</option>
-                        <option value="bug">Bug</option>
-                        <option value="ghost">Ghost</option>
-                        <option value="steel">Steel</option>
-                        <option value="fire">Fire</option>
-                        <option value="water">Water</option>
-                        <option value="grass">Grass</option>
-                        <option value="electric">Electric</option>
-                        <option value="psychic">Psychic</option>
-                        <option value="ice">Ice</option>
-                        <option value="dragon">Dragon</option>
-                        <option value="dark">Dark</option>
-                        <option value="fairy">Fairy</option>
-                    </select>
-                </label>
-            </form>
+            <FormSelectType/>
 
             {Object.keys(data).length !== 0 ?
                 <>
