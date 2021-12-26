3680c6e6c4af4366ccb927b90fdd54e676c01046
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-26 13:51:52 +0100

    :bug: fix key child

--- src/containers/Pokemon.jsx
+++ src/containers/Pokemon.jsx
@@ -79,9 +79,8 @@ const Pokemon = () => {
 
                         <ul className="flex flex-row">
                             {pokemon.types.map((type, index) => (
-                                <Link to={`/type/${type.type.name}`}>
-                                    <li key={index}
-                                        className={"capitalize border-2 border-black rounded-xl m-2 p-2 " + color_type[type.type.name]}>
+                                <Link key ={index} to={`/type/${type.type.name}`}>
+                                    <li className={"capitalize border-2 border-black rounded-xl m-2 p-2 " + color_type[type.type.name]}>
                                         {type.type.name}
                                     </li>
                                 </Link>
