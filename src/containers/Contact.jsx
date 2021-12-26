2c962785ba3ea0961b93a9c361d7608329c123e0
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-12-26 13:03:56 +0100

    add placeholder on Contact form

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -38,6 +38,7 @@ const Contact = () => {
                             <div className="md:w-2/3">
                                 <input type="text"
                                        name="name"
+                                       placeholder="Mr Glox"
                                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                                        required/>
                             </div>
@@ -58,6 +59,7 @@ const Contact = () => {
                                        min="12"
                                        max="20"
                                        name="grade"
+                                       placeholder="16"
                                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                                        required/>
                             </div>
